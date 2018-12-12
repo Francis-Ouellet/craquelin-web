79666e35bcb3ba0ba0aaafe991d8e032220d5a3d
Francis Ouellet <francis.ouellet@optelgroup.com>
2018-12-11 19:57:14 -0500

    Base configs

---
+++ test/test.strings.ts
@@ -0,0 +1,8 @@
+import * as assert from "assert";
+import * as string from "./../src/strings";
+
+describe("Testing Strings", () => {
+    it("should display Hello World!", () => {
+        assert.equal(string.HELLO_WORLD, "Hello World");
+    });
+});
