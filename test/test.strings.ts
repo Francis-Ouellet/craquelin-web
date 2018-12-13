e86dc8b722bbe70adb84f6ac720d99c942e51de5
Francis Ouellet <francis.ouellet@optelgroup.com>
2018-12-13 11:22:12 -0500

    source files in src, compiled files in build

--- test/test.strings.ts
+++ test/test.strings.ts
@@ -1,5 +1,5 @@
 import * as assert from "assert";
-import * as string from "./../strings";
+import * as string from "../src/strings";
 
 describe("Testing Strings", () => {
     it("should display Hello World!", () => {
