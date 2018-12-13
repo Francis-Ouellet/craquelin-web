6074bafc0271e5e3becc60590f90fc675996926e
Francis Ouellet <francis.ouellet@optelgroup.com>
2018-12-13 08:13:44 -0500

    Added ! to Hello World

--- test/test.strings.ts
+++ test/test.strings.ts
@@ -3,6 +3,6 @@ import * as string from "./../src/strings";
 
 describe("Testing Strings", () => {
     it("should display Hello World!", () => {
-        assert.equal(string.HELLO_WORLD, "Hello World");
+        assert.equal(string.HELLO_WORLD, "Hello World!");
     });
 });
