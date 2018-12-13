9af7895c88e2b9a583c5cea2df4d6ecddf2d68b5
Francis Ouellet <francis.ouellet@optelgroup.com>
2018-12-13 11:35:48 -0500

    Updated app code, updated start path

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import * as string from "./strings";
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send(string.HELLO_WORLD));
+app.get("/", (req, res) => {
+    res.send(string.HELLO_WORLD);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
